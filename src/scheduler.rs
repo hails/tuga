36960703d61eb40a867fc817371b6a0a943f300a
Allan Jorge <contato.allanj@gmail.com>
2020-03-21 00:22:26 -0300

    add update time as env var

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,12 +1,15 @@
 use crate::process;
+use std::env;
 use std::time::Duration;
 use telegram_bot::types::refs::UserId;
 use telegram_bot::*;
 use tokio;
 
 pub fn start(token: String) {
-    // api.send(request: Req)
     tokio::spawn(async move {
+        let update_in_secs = env::var("UPDATE_DATA_IN_SECS").expect("UPDATE_DATA_IN_SECS not set");
+        let update_in_secs = update_in_secs.parse::<u64>().unwrap();
+
         let api = Api::new(&token);
 
         loop {
@@ -24,7 +27,7 @@ pub fn start(token: String) {
                 );
                 api.send(message).await.unwrap();
             }
-            tokio::time::delay_for(Duration::from_secs(1)).await;
+            tokio::time::delay_for(Duration::from_secs(update_in_secs)).await;
         }
     });
 }
